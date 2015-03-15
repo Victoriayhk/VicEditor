be41e8be2a287d0fa2761661a8781b3a50d4f4e0
Huikang Yi <huikangyi@gmail.com>
2015-03-15 20:59:14 +0800

    showdown added, and it works every good.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Rich-Text-Editor
 
+## 版本说明
+
+### 0.1
+这个版本只有最最基础的UI设计以及简单的iframe contentEditable方式的富文本编辑方式; Bold, Italic等按钮的标签使用的是从从某个icon全图中抠出来的18*18像素的图, 不利于扩展
+
+### 1.0
+这个版本引用了插件[showdown][1], 基本实现了Markdown到HTML的转换, 见index.html示例; 另外, 按钮标签改成了[Font Awesome][2]
+
+未完成的事情有:
+
++ 按钮事件的定制
++ CSS样式定制
+
+
+[1]: https://github.com/showdownjs/showdown
+[2]: http://www.thinkcmf.com/font/icons#new
--- editor.js
+++ editor.js
@@ -1,39 +1,12 @@
 window.onload = function() {
-	var editor 	 = document.getElementById("editor");
-	var buttons  = editor.getElementsByTagName("span");
-	var selects  = editor.getElementsByTagName("select");
-
-	var console = document.getElementById("console");
-	// console.style.display = "none";
-	var iframe = document.createElement("iframe");
-	iframe.style.width = "100%";
-	iframe.height = "240px";
-	iframe.frameBorder = 0;
-	console.parentNode.insertBefore(iframe, console);
-	var iframeDocument = iframe.contentDocument || iframe.contentWindow.docment;
-	iframeDocument.designMode = "on";
-	iframeDocument.open();
-	iframeDocument.write('<html><head></head><body></body></html>');
-	iframeDocument.close();
-
-
-	for(var i = 0,l = buttons.length; i < l; i++) {
-	   	buttons[i].onclick = new function() {
-	        var command = buttons[i].getAttribute("title");
-	        return function() {
-	        	iframeDocument.execCommand(command);
-	        }
+	var converter = new Showdown.converter({ extensions: ['twitter'] });
+	var input = document.getElementById('textarea');
+	var submitbutton = document.getElementById('submit');
+	
+	submitbutton.onclick = new function() {
+	    return function() {
+	        document.getElementById("exportresult").innerHTML =
+	          converter.makeHtml(input.value);
 	    }
-	}
-
-	for (var i = 0, l = selects.length; i < l; i++) {
-		selects[i].onchange = new function() {
-			var select = selects[i];
-			return function() {
-				var command = select.getAttribute("title");
-				value = select.options[select.selectedIndex].value;
-				iframeDocument.execCommand(command, false, value);
-			}
-		}
-	}
+	};
 }
