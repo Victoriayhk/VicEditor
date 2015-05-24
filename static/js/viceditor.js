9b1a1503327b58a5e4b4f183bf759aac61bf07bc
Huikang Yi <huikangyi@gmail.com>
2015-05-24 10:33:20 +0800

    + doge added, no bug anymore + README updated

--- static/js/viceditor.js
+++ static/js/viceditor.js
@@ -1,3 +1,53 @@
+//                        ::
+//                        :;J7, :,                        ::;7:
+//                        ,ivYi, ,                       ;LLLFS:
+//                        :iv7Yi                       :7ri;j5PL
+//                       ,:ivYLvr                    ,ivrrirrY2X,
+//                       :;r@Wwz.7r:                :ivu@kexianli.
+//                      :iL7::,:::iiirii:ii;::::,,irvF7rvvLujL7ur
+//                     ri::,:,::i:iiiiiii:i:irrv177JX7rYXqZEkvv17
+//                  ;i:, , ::::iirrririi:i:::iiir2XXvii;L8OGJr71i
+//                :,, ,,:   ,::ir@mingyi.irii:i:::j1jri7ZBOS7ivv,
+//                   ,::,    ::rv77iiiriii:iii:i::,rvLq@huhao.Li
+//               ,,      ,, ,:ir7ir::,:::i;ir:::i:i::rSGGYri712:
+//             :::  ,v7r:: ::rrv77:, ,, ,:i7rrii:::::, ir7ri7Lri
+//            ,     2OBBOi,iiir;r::        ,irriiii::,, ,iv7Luur:
+//          ,,     i78MBBi,:,:::,:,  :7FSL: ,iriii:::i::,,:rLqXv::
+//          :      iuMMP: :,:::,:ii;2GY7OBB0viiii:i:iii:i:::iJqL;::
+//         ,     ::::i   ,,,,, ::LuBBu BBBBBErii:i:i:i:i:i:i:r77ii
+//        ,       :       , ,,:::rruBZ1MBBqi, :,,,:::,::::::iiriri:
+//       ,               ,,,,::::i:  @arqiao.       ,:,, ,:::ii;i7:
+//      :,       rjujLYLi   ,,:::::,:::::::::,,   ,:i,:,,,,,::i:iii
+//      ::      BBBBBBBBB0,    ,,::: , ,:::::: ,      ,,,, ,,:::::::
+//      i,  ,  ,8BMMBBBBBBi     ,,:,,     ,,, , ,   , , , :,::ii::i::
+//      :      iZMOMOMBBM2::::::::::,,,,     ,,,,,,:,,,::::i:irr:i:::,
+//      i   ,,:;u0MBMOG1L:::i::::::  ,,,::,   ,,, ::::::i:i:iirii:i:i:
+//      :    ,iuUuuXUkFu7i:iii:i:::, :,:,: ::::::::i:i:::::iirr7iiri::
+//      :     :rk@Yizero.i:::::, ,:ii:::::::i:::::i::,::::iirrriiiri::,
+//       :      5BMBBBBBBSr:,::rv2kuii:::iii::,:i:,, , ,,:,:i@petermu.,
+//            , :r50EZ8MBBBBGOBBBZP7::::i::,:::::,: :,:,::i;rrririiii::
+//                :jujYY7LS0ujJL7r::,::i::,::::::::::::::iirirrrrrrr:ii:
+//             ,:  :@kevensun.:,:,,,::::i:i:::::,,::::::iir;ii;7v77;ii;i,
+//             ,,,     ,,:,::::::i:iiiii:i::::,, ::::iiiir@xingjief.r;7:i,
+//          , , ,,,:,,::::::::iiiiiiiiii:,:,:::::::::iiir;ri7vL77rrirri::
+//           :,, , ::::::::i:::i:::i:i::,,,,,:,::i:i:::iir;@Secbone.ii:::
+
+       
+//     _________      ___    
+//     | ______ \    / _ \
+//     | |____| |   / / \ \   ___________  ______     ______ 
+//     |  __  __/  / /___\ \  | __   __  \ | ___ \   | ____ \
+//     | |  \ \   |  _____  | | | | |  | | | |__\_\  | |   | |
+//     | |   \ \  | |     | | | | | |  | | | |_____  | |   | |
+//     |_|    \_\ |_|     |_| |_| |_|  |_| |_______| |_|   |_|
+//
+//      天上辣么多神，总有一个会助我写代码不生BUG
+//
+//		Author     : yihuikang
+//		Email      : huikangyi@gmail.com
+//      Last-update: 2015-05-24 10:04
+
+
 /**************************************************************************************
 ! jQuery v2.1.3 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license
 ***************************************************************************************/
